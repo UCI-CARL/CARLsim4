73fb3e07958e55889e2dc78721c0a7497e9f6726
tingshuc <tingshuc@uci.edu>
2014-09-02 15:05:24 -0700

    First try for I-STDP example

--- examples/istdp/main_istdp.cpp
+++ examples/istdp/main_istdp.cpp
@@ -45,44 +45,7 @@
 	#define _CRT_SECURE_NO_WARNINGS
 #endif
 
-#define NUM_DA_NEURON 30
-#define NUM_NEURON 10
-
-class TwoGroupsSpikeController: public SpikeGenerator {
-private:
-	int isi;
-	int offset;
-	int g1;
-	int g2;
-	bool setOffset;
-public:
-	TwoGroupsSpikeController(int interSpikeInterval, int offsetToFirstGroup, int firstGroup, int secondGroup) {
-		isi = interSpikeInterval;
-		offset = offsetToFirstGroup;
-		g1 = firstGroup;
-		g2 = secondGroup;
-		setOffset = false;
-	}
-
-	unsigned int nextSpikeTime(CARLsim* s, int grpId, int nid, unsigned int currentTime, unsigned int lastScheduledSpikeTime) {
-		if (grpId == g1) // gin
-			return lastScheduledSpikeTime + isi;
-		else if (grpId == g2) { // gex
-			if (!setOffset) {
-				setOffset = true;
-				return lastScheduledSpikeTime + isi + offset;
-			} else
-				return lastScheduledSpikeTime + isi;
-		}
-
-		return 0xFFFFFFFF;
-	}
-
-	void updateOffset(int newOffset) {
-		offset = newOffset;
-		setOffset = false;
-	}
-};
+#define NUM_NEURON 50
 
 int main()
 {
@@ -92,68 +55,100 @@ int main()
 	std::vector<int> spikesPre;
 	float* weights = NULL;
 	int size;
-	SpikeMonitor* spikeMon1;
-	SpikeMonitor* spikeMonIn;
+	SpikeMonitor* spikeMonIn1;
+	SpikeMonitor* spikeMonIn2;
 	SpikeMonitor* spikeMonEx;
-	TwoGroupsSpikeController* spikeCtrl;
-	int gin, gex, g1;
+	int gEx, gIn1, gIn2, gInput1, gInput2, gInput3, gInput4;
 	float BETA_LTP = 0.10f/100;
-	float BETA_LTD = 0.14f/100;
-	float LAMDA = 10.0f;
-	float DELTA = 40.0f;
+	float BETA_LTD = 0.12f/100;
+	float LAMDA = 6.0f;
+	float DELTA = 20.0f;
+	float ALPHA_LTP = 0.10f/100;
+	float ALPHA_LTD = 0.12f/100;
+	float TAU_LTP = 20.0f;
+	float TAU_LTD = 20.0f;
 	//FILE* fid = fopen("results/weight.csv", "w");
 
 	// create a network
 	CARLsim sim("istdp",GPU_MODE, USER,0,1,42);
 
-	g1=sim.createGroup("excit", 1, EXCITATORY_NEURON);
-	sim.setNeuronParameters(g1, 0.02f, 0.2f, -65.0f, 8.0f);
+	gEx = sim.createGroup("excit", 50, EXCITATORY_NEURON);
+	sim.setNeuronParameters(gEx, 0.02f, 0.2f, -65.0f, 8.0f);
 
-	gex=sim.createSpikeGeneratorGroup("input-ex", 1, EXCITATORY_NEURON);
-	gin=sim.createSpikeGeneratorGroup("input-in", 1, INHIBITORY_NEURON);
+	gIn1 = sim.createGroup("inhib", 50, INHIBITORY_NEURON);
+	sim.setNeuronParameters(gIn1, 0.1f,  0.2f, -65.0f, 2.0f);
 
-	spikeCtrl = new TwoGroupsSpikeController(100, 15, gin, gex);
+	gIn2 = sim.createGroup("inhib", 50, INHIBITORY_NEURON);
+	sim.setNeuronParameters(gIn2, 0.1f,  0.2f, -65.0f, 2.0f);
 
-	sim.connect(gex, g1, "one-to-one", RangeWeight(40.0f/100), 1.0f, RangeDelay(1), SYN_FIXED);
-	sim.connect(gin, g1, "one-to-one", RangeWeight(0.0, 5.0f/100, 10.0f/100), 1.0f, RangeDelay(1), SYN_PLASTIC);
+	gInput1=sim.createSpikeGeneratorGroup("input_1", 50, EXCITATORY_NEURON);
+	gInput2=sim.createSpikeGeneratorGroup("input_2", 50, EXCITATORY_NEURON);
+	gInput3=sim.createSpikeGeneratorGroup("input_3", 50, EXCITATORY_NEURON);
+	gInput4=sim.createSpikeGeneratorGroup("input_4", 50, EXCITATORY_NEURON);
+
+	sim.connect(gInput1, gEx, "full", RangeWeight(0.0, 1.0f/100, 2.0f/100), 1.0f, RangeDelay(1, 5), SYN_PLASTIC);
+	sim.connect(gInput1, gIn1, "full", RangeWeight(0.0, 0.9f/100, 1.2f/100), 1.0f, RangeDelay(15, 20), SYN_PLASTIC);
+	sim.connect(gInput1, gIn2, "full", RangeWeight(0.0, 0.9f/100, 1.2f/100), 1.0f, RangeDelay(15, 20), SYN_PLASTIC);
+
+	sim.connect(gInput2, gEx, "full", RangeWeight(0.0, 1.0f/100, 2.0f/100), 1.0f, RangeDelay(1), SYN_PLASTIC);
+	sim.connect(gInput3, gIn1, "full", RangeWeight(0.0, 0.9f/100, 1.2f/100), 1.0f, RangeDelay(1), SYN_PLASTIC);
+	sim.connect(gInput4, gIn2, "full", RangeWeight(0.0, 0.9f/100, 1.2f/100), 1.0f, RangeDelay(1), SYN_PLASTIC);
+
+	sim.connect(gIn1, gEx, "full", RangeWeight(0.0, 0.5f/100, 1.0f/100), 1.0f, RangeDelay(1), SYN_PLASTIC);
+	sim.connect(gIn2, gEx, "full", RangeWeight(0.0, 0.5f/100, 1.0f/100), 1.0f, RangeDelay(1), SYN_PLASTIC);
 
 	// enable COBA, set up STDP, enable dopamine-modulated STDP
 	sim.setConductances(true,5,150,6,150);
-	sim.setISTDP(g1, true, STANDARD, BETA_LTP, BETA_LTD, LAMDA, DELTA);
+	sim.setESTDP(gEx, true, STANDARD, ALPHA_LTP, TAU_LTP, ALPHA_LTD, TAU_LTD);
+	sim.setISTDP(gEx, true, STANDARD, BETA_LTP, BETA_LTD, LAMDA, DELTA);
 
-	// set up spike controller on DA neurons
-	sim.setSpikeGenerator(gex, spikeCtrl);
-	sim.setSpikeGenerator(gin, spikeCtrl);
+	sim.setESTDP(gIn1, true, STANDARD, ALPHA_LTP, TAU_LTP, ALPHA_LTD, TAU_LTD);
+	sim.setESTDP(gIn2, true, STANDARD, ALPHA_LTP, TAU_LTP, ALPHA_LTD, TAU_LTD);
 
 	// build the network
 	sim.setupNetwork();
 
-	spikeMon1 = sim.setSpikeMonitor(g1);
-	spikeMonIn = sim.setSpikeMonitor(gin);
-	spikeMonEx = sim.setSpikeMonitor(gex);
+	spikeMonEx = sim.setSpikeMonitor(gEx);
+	spikeMonIn1 = sim.setSpikeMonitor(gIn1);
+	spikeMonIn2 = sim.setSpikeMonitor(gIn2);
+
+	//setup some baseline input
+	PoissonRate in1(NUM_NEURON);
+	for (int i = 0; i < NUM_NEURON; i++) in1.rates[i] = 2;
+		sim.setSpikeRate(gInput1, &in1);
+
+	PoissonRate in2(NUM_NEURON);
+	for (int i = 0; i < NUM_NEURON; i++) in2.rates[i] = 2;
+		sim.setSpikeRate(gInput2, &in2);
+
+	PoissonRate in3(NUM_NEURON);
+	for (int i = 0; i < NUM_NEURON; i++) in3.rates[i] = 2;
+		sim.setSpikeRate(gInput3, &in3);
+
+	PoissonRate in4(NUM_NEURON);
+	for (int i = 0; i < NUM_NEURON; i++) in4.rates[i] = 2;
+		sim.setSpikeRate(gInput4, &in4);
 
 
 	// run for 1000 seconds
-	for (int t = 0; t < 20; t++) {
-		spikeMon1->startRecording();
-		spikeMonIn->startRecording();
+	for (int t = 0; t < 1000; t++) {
+		spikeMonIn1->startRecording();
+		spikeMonIn2->startRecording();
 		spikeMonEx->startRecording();
 		sim.runNetwork(1,0,true, true);
-		spikeMon1->stopRecording();
-		spikeMonIn->stopRecording();
+		spikeMonIn1->stopRecording();
+		spikeMonIn2->stopRecording();
 		spikeMonEx->stopRecording();
 		
 		//spikeMonIn->print();
 		//spikeMonEx->print();
 		//spikeMon1->print();
 
-		sim.getPopWeights(gin, g1, weights, size);
-		printf("%f\n",weights[0]);
+		//sim.getPopWeights(gin, g1, weights, size);
+		//printf("%f\n",weights[0]);
 	}
 
 	//fclose(fid);
-
-	delete spikeCtrl;
 
 	return 0;
 }
