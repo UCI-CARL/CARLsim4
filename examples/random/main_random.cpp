517142fd7526f9f6ef08f01c9a14f2380ff4e352
tingshuc <tingshuc@uci.edu>
2014-11-14 01:41:29 -0800

    Restore main_random.cpp

--- examples/random/main_random.cpp
+++ examples/random/main_random.cpp
@@ -39,160 +39,61 @@
  */
 
 #include <carlsim.h>
-#include <stdio.h>
 
 #if (WIN32 || WIN64)
 	#define _CRT_SECURE_NO_WARNINGS
 #endif
 
-#define numGPU 2
-
-void *RunNetwork(void* arg) {
-	CARLsim* sim = (CARLsim*)arg;
-
-	for (int t = 0; t < 100; t++)
-		sim->runNetwork(1, 0);
-}
-
 int main() {
+	// simulation details
+	int N = 1000; // number of neurons
+	int ithGPU = 0; // run on first GPU
 
-	CARLsim** network;
-	pthread_t* threads;
+	// create a network
+	CARLsim sim("random",GPU_MODE,USER,ithGPU,42);
 
-	network = new CARLsim*[2];
-	threads = new pthread_t[2];
+	int g1=sim.createGroup("excit", N*0.8, EXCITATORY_NEURON);
+	sim.setNeuronParameters(g1, 0.02f, 0.2f, -65.0f, 8.0f);
 
-	/** construct a CARLsim network on the heap. */
-	network[0] = new CARLsim("tunePolyGroupECJ", GPU_MODE, USER, 0);
-	network[1] = new CARLsim("tunePolyGroupECJ", GPU_MODE, USER, 1);
+	int g2=sim.createGroup("inhib", N*0.2, INHIBITORY_NEURON);
+	sim.setNeuronParameters(g2, 0.1f,  0.2f, -65.0f, 2.0f);
 
-	const float COND_tAMPA=5.0, COND_tNMDA=150.0, COND_tGABAa=6.0, COND_tGABAb=150.0;
+	int gin=sim.createSpikeGeneratorGroup("input",N*0.1,EXCITATORY_NEURON);
 
-	// Neurons
-	const int NUM_NEURONS_EXC = 800;
-	const int NUM_NEURONS_INB = 200;
-	const float alphaLTP = 0.1f/100;
-	const float alphaLTD = 0.12f/100;
-	const float tauLTP = 20.0f;
-	const float tauLTD = 20.0f;
-	const float betaLTP = 0.10f/100;
-	const float betaLTD = 0.06f/100;
-	const float lamda = 6.0f;
-	const float delta = 20.0f;
+	sim.setConductances(true,5,150,6,150);
 
-	// Simulation time (each must be at least 1s due to bug in SpikeMonitor)
-	const int runTime = 2;
+	// make random connections with 10% probability
+	sim.connect(g2,g1,"random", RangeWeight(0.01), 0.1f);
+	// make random connections with 10% probability, and random delays between 1 and 20
+	sim.connect(g1,g2,"random", RangeWeight(0.0,0.0025,0.005), 0.1f, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+	sim.connect(g1,g1,"random", RangeWeight(0.0,0.06,0.1), 0.1f, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
 
-	// Target rates for the objective function
-	const float INPUT_HZ = 1.0f;
-	const float EXC_TARGET_HZ   = 1.0f;
-	//const float INH_TARGET_HZ   = 2.0f;
+	// 5% probability of connection
+	sim.connect(gin, g1, "random", RangeWeight(1.0), 0.05f, RangeDelay(1,20), RadiusRF(-1));
 
-	int indiNum = 4;
-
-	int poissonGroup[indiNum];
-	int excGroup[indiNum];
-	int inhGroup[indiNum];
-	SpikeMonitor* excMonitor[indiNum];
-	//SpikeMonitor* inhMonitor[indiNum];
-	float excHz[indiNum];
-	//float inhHz[indiNum];
-	float excError[indiNum];
-	//float inhError[indiNum];
-	float fitness[indiNum];
-	int startIdx[2];
-	int endIdx[2];
-
-	FILE* fd = fopen("debug2.log", "a");
-	fprintf(fd, "indiNum:%d\n", indiNum);
-
-	startIdx[0] = 0;
-	endIdx[1] = indiNum;
-	startIdx[1] = indiNum / numGPU;
-	endIdx[0] = indiNum / numGPU;
-	// NumInstances -> number of individuals
-	for (int gpuId = 0; gpuId < numGPU; gpuId++) {
-		for(unsigned int i = startIdx[gpuId]; i < endIdx[gpuId]; i++) {
-			fprintf(fd, "gpuId %d, i %d\n", gpuId, i);
-			/** Decode a genome*/
-			poissonGroup[i] = network[gpuId]->createSpikeGeneratorGroup("poisson", NUM_NEURONS_EXC, EXCITATORY_NEURON);
-			excGroup[i] = network[gpuId]->createGroup("exc", NUM_NEURONS_EXC, EXCITATORY_NEURON);
-			inhGroup[i] = network[gpuId]->createGroup("inh", NUM_NEURONS_INB, INHIBITORY_NEURON);
-			network[gpuId]->setNeuronParameters(excGroup[i], 0.02f, 0.2f, -65.0f, 8.0f);
-			network[gpuId]->setNeuronParameters(inhGroup[i], 0.1f, 0.2f, -65.0f, 2.0f);
-			network[gpuId]->setConductances(true,COND_tAMPA,COND_tNMDA,COND_tGABAa,COND_tGABAb);
-
-			network[gpuId]->connect(poissonGroup[i], excGroup[i], "one-to-one", RangeWeight(12.0f/100), 1.0f, RangeDelay(1), RadiusRF(-1), SYN_FIXED);
-			network[gpuId]->connect(excGroup[i], excGroup[i], "random", RangeWeight(4.0f/100), 0.1f, RangeDelay(1, 20), RadiusRF(-1), SYN_PLASTIC);
-			network[gpuId]->connect(excGroup[i], inhGroup[i], "random", RangeWeight(4.0f/100), 0.1f, RangeDelay(1, 20), RadiusRF(-1), SYN_PLASTIC);
-			network[gpuId]->connect(inhGroup[i], excGroup[i], "random", RangeWeight(4.0f/100), 0.1f, RangeDelay(1), RadiusRF(-1), SYN_FIXED);
-
-			network[gpuId]->setSTDP(excGroup[i], true, STANDARD, alphaLTP, tauLTP, alphaLTD, tauLTD);
-		}
-	}
-
-	// can't call setupNetwork() multiple times in the loop
-	network[0]->setupNetwork();
-	network[1]->setupNetwork();
-
-	// it's unnecessary to do this in the loop
-	PoissonRate* const in = new PoissonRate(NUM_NEURONS_EXC);
-	for (int k=0;k<NUM_NEURONS_EXC;k++)
-		in->rates[k] = INPUT_HZ;
-
-	for (int gpuId = 0; gpuId < numGPU; gpuId++) {
-		for(unsigned int i = startIdx[gpuId]; i < endIdx[gpuId]; i++) {
-			fprintf(fd, "%d %d\n", gpuId, i);
-			network[gpuId]->setSpikeRate(poissonGroup[i],in);
-
-			excMonitor[i] = network[gpuId]->setSpikeMonitor(excGroup[i], "/dev/null");
-			//inhMonitor[i] = network->setSpikeMonitor(inhGroup[i], "/dev/null");
-
-			// initialize all the error and fitness variables
-			excHz[i]=0; //inhHz[i]=0;
-			excError[i]=0; //inhError[i]=0;
-			fitness[i]=0;
-		}
-	}
-
-	//fprintf(fd, "0\n");
-	//network[0]->runNetwork(runTime,0);
-	//fprintf(fd, "1\n");
-	//network[1]->runNetwork(runTime,0);
-	//for (int t = 0; t < 100; t++) {
-	//	network[0]->runNetwork(1,0);
-	//	network[1]->runNetwork(1, 0);
-	//}
-
-	pthread_create(&threads[0], NULL, RunNetwork, (void*)network[0]);
-	pthread_create(&threads[1], NULL, RunNetwork, (void*)network[1]);
-	pthread_join(threads[0], NULL);
-	pthread_join(threads[1], NULL);
-
-	fprintf(fd, "a\n");
-
-	// Evaluate last 5 second
-	for(unsigned int i = 0; i < indiNum; i++) {
-		excMonitor[i]->startRecording();
-		//inhMonitor[i]->startRecording();
-	}
-
-	fprintf(fd, "b\n");
-
-	network[0]->runNetwork(5,0);
-	network[1]->runNetwork(5,0);
-	//pthread_create(&threads[0], NULL, TunePolyGroupECJExperiment::EvalNetwork, (void*)network[0]);
-	//pthread_create(&threads[1], NULL, TunePolyGroupECJExperiment::RunNetwork, NULL);
-	//pthread_join(threads[0], NULL);
-
-	fprintf(fd, "c\n");
+	// here we define and set the properties of the STDP.
+	float ALPHA_LTP = 0.10f/100, TAU_LTP = 20.0f, ALPHA_LTD = 0.12f/100, TAU_LTD = 20.0f;
+	sim.setSTDP(g1, true, STANDARD, ALPHA_LTP, TAU_LTP, ALPHA_LTD, TAU_LTD);
 
 
-	delete in;
-	fclose(fd);
-	delete network[0];
-	delete network[1];
-	delete [] threads;
-	delete [] network;
+	// build the network
+	sim.setupNetwork();
+	sim.setSpikeMonitor(g1); // put spike times into file
+	sim.setSpikeMonitor(g2); // Show basic statistics about g2
+	sim.setSpikeMonitor(gin);
+
+	sim.setConnectionMonitor(g1, g2);
+
+	//setup some baseline input
+	PoissonRate in(N*0.1);
+	for (int i=0;i<N*0.1;i++) in.rates[i] = 1;
+		sim.setSpikeRate(gin,&in);
+
+	// run for a total of 10 seconds
+	// at the end of each runNetwork call, SpikeMonitor stats will be printed
+	for (int i=0; i<10; i++)
+		sim.runNetwork(1,0);
+
+	return 0;
 }
 
