e0327bfa8e65192dbecdcf18a8cbf9fdc5195f36
tingshuc <tingshuc@uci.edu>
2014-11-26 23:57:34 -0800

    1. Add protection header macro into carlsim_tests.h and spikegen*.h 2. Remove spikeCounter.cpp, which is replace by spike_counter.cpp 3. Create interactive_spikegen.cpp(.h) and pre_post_group_spikegen.cpp(.h) for stdp test cases

--- tools/spike_generators/pre_post_group_spikegen.h
+++ tools/spike_generators/pre_post_group_spikegen.h
@@ -36,55 +36,29 @@
  *					(TSC) Ting-Shuo Chou <tingshuc@uci.edu>
  *
  * CARLsim available from http://socsci.uci.edu/~jkrichma/CARLsim/
- * Ver 11/28/2014
- */
-#ifndef _PERIODIC_SPIKEGEN_H_
-#define _PERIODIC_SPIKEGEN_H_
+ * Ver 11/26/2014
+ */ 
+#ifndef _PRE_POST_GROUP_SPIKEGEN_H_
+#define _PRE_POST_GROUP_SPIKEGEN_H_
 
 #include <callback.h>
-#include <vector>
 
 /*!
- * \brief a periodic SpikeGenerator (constant ISI) creating spikes at a certain rate
- *
- * This class implements a period SpikeGenerator that schedules spikes with a constant inter-spike-interval.
- * For example, a PeriodicSpikeGenerator with rate=10Hz will schedule spikes for each neuron in the group at t=100,
- * t=200, t=300, etc. If spikeAtZero is set to true, then the first spike will be scheduled at t=0.
+ * \brief spike generator for spike timing control
  */
-class PeriodicSpikeGenerator : public SpikeGenerator {
-public:
-	/*!
-	 * \brief PeriodicSpikeGenerator constructor
-	 * \param[in] rate the firing rate (Hz) at which to schedule spikes
-	 * \param[in] spikeAtZero a boolean flag to indicate whether to insert the first spike at t=0
-	 */
-	PeriodicSpikeGenerator(float rate, bool spikeAtZero=true);
-
-	//! PeriodicSpikeGenerator destructor
-	~PeriodicSpikeGenerator() {}
-
-	/*!
-	 * \brief schedules the next spike time
-	 *
-	 * This function schedules the next spike time, given the currentTime and the lastScheduledSpikeTime. It implements
-	 * the virtual function of the base class.
-	 * \param[in] sim pointer to a CARLsim object
-	 * \param[in] grpId current group ID for which to schedule spikes
-	 * \param[in] nid current neuron ID for which to schedule spikes
-	 * \param[in] currentTime current time (ms) at which spike scheduler is called
-	 * \param[in] lastScheduledSpikeTime the last time (ms) at which a spike was scheduled for this nid, grpId
-	 * \returns the next spike time (ms)
-	 */
-	unsigned int nextSpikeTime(CARLsim* sim, int grpId, int nid, unsigned int currentTime, 
-		unsigned int lastScheduledSpikeTime);
-
+class PrePostGroupSpikeGenerator: public SpikeGenerator {
 private:
-	void checkFiringRate();
-	
-	float rate_;		//!< spike rate (Hz)
-	int isi_;			//!< inter-spike interval that results in above spike rate
-	std::vector<int> nIdFiredAtZero_; //!< keep track of all neuron IDs for which a spike at t=0 has been scheduled
-	bool spikeAtZero_; //!< whether to emit a spike at t=0
+	int isi;
+	int offset;
+	int g1;
+	int g2;
+	bool setOffset;
+public:
+	PrePostGroupSpikeGenerator(int interSpikeInterval, int offsetToFirstGroup, int firstGroup, int secondGroup);
+
+	unsigned int nextSpikeTime(CARLsim* s, int grpId, int nid, unsigned int currentTime, unsigned int lastScheduledSpikeTime);
+
+	void updateOffset(int newOffset);
 };
 
 #endif
