196d29c56e1b122dcc7d729cd085696a4e6d1804
staslist <slistopa@uci.edu>
2017-05-10 23:58:58 -0700

    Still cannot manually reproduce the divergence between CPU & GPU spiketimes observed in compartments.cpp CPU vs GPU test at timestep = 30.

--- projects/test_kernel/src/main_test_kernel.cpp
+++ projects/test_kernel/src/main_test_kernel.cpp
@@ -169,89 +169,165 @@ int main() {
 
 	// Test Compartments
 
-	CARLsim* sim = new CARLsim("test kernel", GPU_MODE, USER, 0, 42);
-	int numIntSteps = 100;
-	sim->setIntegrationMethod(RUNGE_KUTTA4, numIntSteps);
+for (int hasCOBA = 0; hasCOBA <= 1; hasCOBA++) {
+	int cpu_numSpikesSP, cpu_numSpikesSR, cpu_numSpikesSLM, cpu_numSpikesSO;
+	std::vector<std::vector<int> > cpu_spkTimesSP, cpu_spkTimesSR, cpu_spkTimesSLM, cpu_spkTimesSO;
+	for (int numIntSteps = 10; numIntSteps <= 100; numIntSteps += 10) {
+		for (int isGPUmode = 0; isGPUmode <= 1; isGPUmode++) {
+			CARLsim* sim = new CARLsim("COMPARTMENTS.spikeTimesCPUvsGPU",
+				isGPUmode ? GPU_MODE : CPU_MODE, SILENT, 0, 42);
+			sim->setIntegrationMethod(RUNGE_KUTTA4, numIntSteps);
+			printf("This simulation has %i steps.\n", numIntSteps);
 
-	int N = 5;
+			int N = 5;
 
-	int grpSP = sim->createGroup("SP soma", N, EXCITATORY_NEURON, 0, GPU_CORES); // s
-	int grpSR = sim->createGroup("SR d1", N, EXCITATORY_NEURON, 0, GPU_CORES); // d1
-	int grpSLM = sim->createGroup("SLM d2", N, EXCITATORY_NEURON, 0, GPU_CORES); // d2
-	int grpSO = sim->createGroup("SO d3", N, EXCITATORY_NEURON, 0, GPU_CORES); // d3
+			int grpSP = sim->createGroup("excit", N, EXCITATORY_NEURON);
+			int grpSR = sim->createGroup("excit", N, EXCITATORY_NEURON);
+			int grpSLM = sim->createGroup("excit", N, EXCITATORY_NEURON);
+			int grpSO = sim->createGroup("excit", N, EXCITATORY_NEURON);
 
-	sim->setNeuronParameters(grpSP, 550.0f, 2.3330991f, -59.101414f, -50.428886f, 0.0021014998f, -0.41361538f,
-		24.98698f, -53.223213f, 109.0f);//9 parameter setNeuronParametersCall (RS NEURON) (soma)
-	sim->setNeuronParameters(grpSR, 367.0f, 1.1705916f, -59.101414f, -44.298294f, 0.2477681f, 3.3198094f,
-		20.274296f, -46.076824f, 24.0f);//9 parameter setNeuronParametersCall (RS NEURON) (dendr)
-	sim->setNeuronParameters(grpSLM, 425.0f, 2.2577047f, -59.101414f, -25.137894f, 0.32122386f, 0.14995363f,
-		13.203414f, -38.54892f, 69.0f);//9 parameter setNeuronParametersCall (RS NEURON) (dendr)
-	sim->setNeuronParameters(grpSO, 225.0f, 1.109572f, -59.101414f, -36.55802f, 0.29814243f, -4.385603f,
-		21.473854f, -40.343994f, 21.0f);//9 parameter setNeuronParametersCall (RS NEURON) (dendr)
+			sim->setNeuronParameters(grpSP, 550.0f, 2.3330991f, -59.101414f, -50.428886f, 0.0021014998f,
+				-0.41361538f, 24.98698f, -53.223213f, 109.0f);//9 parameter setNeuronParametersCall (RS NEURON) (soma)
+			sim->setNeuronParameters(grpSR, 367.0f, 1.1705916f, -59.101414f, -44.298294f, 0.2477681f,
+				3.3198094f, 20.274296f, -46.076824f, 24.0f);//9 parameter setNeuronParametersCall (RS NEURON) (dendr)
+			sim->setNeuronParameters(grpSLM, 425.0f, 2.2577047f, -59.101414f, -25.137894f, 0.32122386f,
+				0.14995363f, 13.203414f, -38.54892f, 69.0f);//9 parameter setNeuronParametersCall (RS NEURON) (dendr)
+			sim->setNeuronParameters(grpSO, 225.0f, 1.109572f, -59.101414f, -36.55802f, 0.29814243f,
+				-4.385603f, 21.473854f, -40.343994f, 21.0f);//9 parameter setNeuronParametersCall (RS NEURON) (dendr)
 
-	sim->setCompartmentParameters(grpSR, 28.396f, 5.526f);//SR 28 and 5
-	sim->setCompartmentParameters(grpSLM, 50.474f, 0.0f);//SLM 50 and 0
-	sim->setCompartmentParameters(grpSO, 0.0f, 49.14f);//SO 0 and 49
-	sim->setCompartmentParameters(grpSP, 116.861f, 4.60f);// SP (somatic) 116 and 4
+			sim->setCompartmentParameters(grpSR, 28.396f, 5.526f);//SR 28 and 5
+			sim->setCompartmentParameters(grpSLM, 50.474f, 0.0f);//SLM 50 and 0
+			sim->setCompartmentParameters(grpSO, 0.0f, 49.14f);//SO 0 and 49
+			sim->setCompartmentParameters(grpSP, 116.861f, 4.60f);// SP (somatic) 116 and 4
 
-	int gin = sim->createSpikeGeneratorGroup("input", N, EXCITATORY_NEURON);
-	sim->connect(gin, grpSP, "one-to-one", RangeWeight(0.0f), 1.0f, RangeDelay(1), RadiusRF(-1));
+			int gin = sim->createSpikeGeneratorGroup("input", N, EXCITATORY_NEURON);
+			sim->connect(gin, grpSP, "one-to-one", RangeWeight(0.0f), 1.0f, RangeDelay(1), RadiusRF(-1));
 
-	sim->setConductances(false);//This forces use of CUBA model.
+			sim->setConductances(hasCOBA);
 
-								// Establish compartmental connections in order to form the following configuration:
-								//	d3    SO
-								//	|     |
-								//	s     SP
-								//	|     |
-								//	d1    SR
-								//	|     |
-								//	d2    SLM
-	sim->connectCompartments(grpSLM, grpSR);
-	sim->connectCompartments(grpSR, grpSP);
-	sim->connectCompartments(grpSP, grpSO);
+			sim->connectCompartments(grpSLM, grpSR);
+			sim->connectCompartments(grpSR, grpSP);
+			sim->connectCompartments(grpSP, grpSO);
 
-	sim->setESTDP(ALL, false);
-	sim->setISTDP(ALL, false);
+			sim->setSTDP(grpSR, false);
+			sim->setSTDP(grpSLM, false);
+			sim->setSTDP(grpSO, false);
+			sim->setSTDP(grpSP, false);
 
-	sim->setupNetwork();
+			sim->setupNetwork();
 
-	SpikeMonitor* spikeSP = sim->setSpikeMonitor(grpSP, "DEFAULT"); // put spike times into file
-	SpikeMonitor* spikeSR = sim->setSpikeMonitor(grpSR, "DEFAULT"); // put spike times into file
-	SpikeMonitor* spikeSLM = sim->setSpikeMonitor(grpSLM, "DEFAULT"); // put spike times into file
-	SpikeMonitor* spikeSO = sim->setSpikeMonitor(grpSO, "DEFAULT"); // put spike times into file
+			SpikeMonitor* spkMonSP = sim->setSpikeMonitor(grpSP, "DEFAULT"); // put spike times into file
+			SpikeMonitor* spkMonSR = sim->setSpikeMonitor(grpSR, "DEFAULT"); // put spike times into file
+			SpikeMonitor* spkMonSLM = sim->setSpikeMonitor(grpSLM, "DEFAULT"); // put spike times into file
+			SpikeMonitor* spkMonSO = sim->setSpikeMonitor(grpSO, "DEFAULT"); // put spike times into file
 
-	PoissonRate in(N);
+			PoissonRate in(N);
+			in.setRates(0.0f);
+			sim->setSpikeRate(gin, &in);//Inactive input group
 
-	in.setRates(0.0f);
-	sim->setSpikeRate(gin, &in);//Inactive input group
+			spkMonSP->startRecording();
+			spkMonSR->startRecording();
+			spkMonSLM->startRecording();
+			spkMonSO->startRecording();
+			//sim->setExternalCurrent(grpSP, 0);
+			//sim->runNetwork(0, 100);
+			//sim->setExternalCurrent(grpSP, 592);
+			//sim->runNetwork(0, 400);
+			//sim->setExternalCurrent(grpSP, 592);
+			//sim->runNetwork(0, 400);
+			//sim->setExternalCurrent(grpSP, 0);
+			//sim->runNetwork(0, 100);
 
-	spikeSP->startRecording();
-	spikeSR->startRecording();
-	spikeSLM->startRecording();
-	spikeSO->startRecording();
-	//sim->setExternalCurrent(grpSP, 0);
-	//sim->runNetwork(0, 100);
-	//sim->setExternalCurrent(grpSP, 592);
-	//sim->runNetwork(0, 400);
-	//sim->setExternalCurrent(grpSP, 592);
-	//sim->runNetwork(0, 400);
-	//sim->setExternalCurrent(grpSP, 0);
-	//sim->runNetwork(0, 100);
+			sim->setExternalCurrent(grpSP, 600);
+			sim->runNetwork(1, 0);
 
-	sim->setExternalCurrent(grpSP, 600);
-	sim->runNetwork(1, 0);
+			spkMonSP->stopRecording();
+			spkMonSR->stopRecording();
+			spkMonSLM->stopRecording();
+			spkMonSO->stopRecording();
 
-	spikeSP->stopRecording();
-	spikeSR->stopRecording();
-	spikeSLM->stopRecording();
-	spikeSO->stopRecording();
+			spkMonSP->print(true);
+			//spikeSR->print(true);
+			//spikeSLM->print(true);
+			spkMonSO->print(true);
 
-	spikeSP->print(true);
-	spikeSR->print(true);
-	spikeSLM->print(true);
-	spikeSO->print(true);
+			if (isGPUmode == 0) {
+				cpu_numSpikesSP = spkMonSP->getPopNumSpikes();
+				cpu_numSpikesSR = spkMonSR->getPopNumSpikes();
+				cpu_numSpikesSLM = spkMonSLM->getPopNumSpikes();
+				cpu_numSpikesSO = spkMonSO->getPopNumSpikes();
+				cpu_spkTimesSP = spkMonSP->getSpikeVector2D();
+				cpu_spkTimesSR = spkMonSR->getSpikeVector2D();
+				cpu_spkTimesSLM = spkMonSLM->getSpikeVector2D();
+				cpu_spkTimesSO = spkMonSO->getSpikeVector2D();
+			}
+			else {
+				//EXPECT_EQ(spkMonSP->getPopNumSpikes(), cpu_numSpikesSP);
+				if (spkMonSP->getPopNumSpikes() == cpu_numSpikesSP) {
+					std::vector<std::vector<int> > gpu_spkTimesSP = spkMonSP->getSpikeVector2D();
+					for (int i = 0; i<cpu_spkTimesSP.size(); i++) {
+						for (int j = 0; j<cpu_spkTimesSP[0].size(); j++) {
+							if (numIntSteps <= 40) {
+								//EXPECT_EQ(gpu_spkTimesSP[i][j], cpu_spkTimesSP[i][j]);
+								//printf("GPU spike time for neuron's # %i, %i th spike is: %i.\n", i, j, gpu_spkTimesSP[i][j]);
+								//printf("CPU spike time for neuron's # %i, %i th spike is: %i.\n", i, j, cpu_spkTimesSP[i][j]);
+								printf("The difference between GPU & CPU spiketimes is: %i.\n", gpu_spkTimesSP[i][j] - cpu_spkTimesSP[i][j]);
+							}
+							else {
+								// at 50 steps and up, we are allowed to get no more than 1 ms deviation
+								//printf("GPU spike time for neuron's # %i, %i th spike is: %i.\n", i, j, gpu_spkTimesSP[i][j]);
+								//printf("CPU spike time for neuron's # %i, %i th spike is: %i.\n", i, j, cpu_spkTimesSP[i][j]);
+								printf("The difference between GPU & CPU spiketimes is: %i.\n", gpu_spkTimesSP[i][j] - cpu_spkTimesSP[i][j]);
+							}
+						}
+					}
+				}
+
+				//EXPECT_EQ(spkMonSR->getPopNumSpikes(), cpu_numSpikesSR);
+				/*if (spkMonSR->getPopNumSpikes() == cpu_numSpikesSR) {
+					std::vector<std::vector<int> > gpu_spkTimesSR = spkMonSR->getSpikeVector2D();
+					for (int i = 0; i<cpu_spkTimesSR.size(); i++) {
+						for (int j = 0; j<cpu_spkTimesSR[0].size(); j++) {
+							EXPECT_EQ(gpu_spkTimesSR[i][j], cpu_spkTimesSR[i][j]);
+						}
+					}
+				}
+
+				//EXPECT_EQ(spkMonSLM->getPopNumSpikes(), cpu_numSpikesSLM);
+				if (spkMonSLM->getPopNumSpikes() == cpu_numSpikesSLM) {
+					std::vector<std::vector<int> > gpu_spkTimesSLM = spkMonSLM->getSpikeVector2D();
+					for (int i = 0; i<cpu_spkTimesSLM.size(); i++) {
+						for (int j = 0; j<cpu_spkTimesSLM[0].size(); j++) {
+							EXPECT_EQ(gpu_spkTimesSLM[i][j], cpu_spkTimesSLM[i][j]);
+						}
+					}
+				}*/
+
+				//EXPECT_EQ(spkMonSO->getPopNumSpikes(), cpu_numSpikesSO);
+				if (spkMonSO->getPopNumSpikes() == cpu_numSpikesSO) {
+					std::vector<std::vector<int> > gpu_spkTimesSO = spkMonSO->getSpikeVector2D();
+					for (int i = 0; i<cpu_spkTimesSO.size(); i++) {
+						for (int j = 0; j<cpu_spkTimesSO[0].size(); j++) {
+							if (numIntSteps <= 40) {
+								//printf("GPU spike time for neuron's # %i, %i th spike is: %i.\n", i, j, gpu_spkTimesSO[i][j]);
+								//printf("CPU spike time for neuron's # %i, %i th spike is: %i.\n", i, j, cpu_spkTimesSO[i][j]);
+								printf("The difference between GPU & CPU spiketimes is: %i.\n", gpu_spkTimesSO[i][j] - cpu_spkTimesSO[i][j]);
+							}
+							else {
+								// at 50 steps and up, we are allowed to get no more than 1 ms deviation
+								//printf("GPU spike time for neuron's # %i, %i th spike is: %i.\n", i, j, gpu_spkTimesSO[i][j]);
+								//printf("CPU spike time for neuron's # %i, %i th spike is: %i.\n", i, j, cpu_spkTimesSO[i][j]);
+								printf("The difference between GPU & CPU spiketimes is: %i.\n", gpu_spkTimesSO[i][j] - cpu_spkTimesSO[i][j]);
+							}
+						}
+					}
+				}
+			}
 	
+			delete sim;
+			}
+		}
+	}
 
 	return 0;
 }
