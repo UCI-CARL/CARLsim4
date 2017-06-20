1c3ccac06acaa8fc7e70eba5f56065e88b5255e7
T. S. Chou <tingshuc@uci.edu>
2017-06-20 15:54:08 -0700

    Update main_neuron_model.cpp for testing the neuron monitor

--- doc/source/tutorial/8_neuron_model/src/main_neuron_model.cpp
+++ doc/source/tutorial/8_neuron_model/src/main_neuron_model.cpp
@@ -51,87 +51,54 @@
 #include <cmath>
 #include <cstdlib>
 
+#define ONE_NEURON 1
 
 int main(int argc, const char* argv[]) {
 	// ---------------- CONFIG STATE -------------------
-	CARLsim sim("spnet", GPU_MODE, USER, 2, 42);
+	CARLsim sim("spnet", HYBRID_MODE, USER, 0, 42);
 
-	int nNeur = 1000;			// number of neurons
-	int nNeurExc = 0.8*nNeur;	// number of excitatory neurons
-	int nNeurInh = 0.2*nNeur;	// number of inhibitory neurons
-	int nSynPerNeur = 100;  	// number of synpases per neuron
-	int maxDelay = 20;      	// maximal conduction delay
+	float wtExc = 40.0f;
 
-	// create 80-20 network with 80% RS and 20% FS neurons
-	int gExc = sim.createGroup("exc", nNeurExc, EXCITATORY_NEURON, 0, GPU_CORES);
+	// create 
+	int gExc = sim.createGroup("exc", ONE_NEURON, EXCITATORY_NEURON, 0, CPU_CORES);
 	sim.setNeuronParameters(gExc, 0.02f, 0.2f, -65.0f, 8.0f); // RS
-	int gInh = sim.createGroup("inh", nNeurInh, INHIBITORY_NEURON, 1, GPU_CORES);
-	sim.setNeuronParameters(gInh, 0.1f, 0.2f, -65.0f, 2.0f); // FS
 
-	// specify connectivity
-	float wtExc = 6.0f;                   // synaptic weight magnitude if pre is exc
-	float wtInh = 5.0f;                   // synaptic weight magnitude if pre is inh (no negative sign)
-	float wtMax = 10.0f;                  // maximum synaptic weight magnitude
-	float pConn = nSynPerNeur*1.0f/nNeur; // connection probability
+	int gInput = sim.createSpikeGeneratorGroup("input", ONE_NEURON, EXCITATORY_NEURON, 0, CPU_CORES);
 
 	// gExc receives input from nSynPerNeur neurons from both gExc and gInh
 	// every neuron in gExc should receive ~nSynPerNeur synapses
-	sim.connect(gExc, gExc, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
-	sim.connect(gInh, gExc, "random", RangeWeight(0.0f, wtInh, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
-
-	// gInh receives input from nSynPerNeur neurons from gExc, all delays are 1ms, no plasticity
-	// every neuron in gInh should receive ~nSynPerNeur synapses
-	sim.connect(gExc, gInh, "random", RangeWeight(wtExc), pConn*nNeur/nNeurExc, RangeDelay(1), RadiusRF(-1), SYN_FIXED);
-
-	// enable STDP on all incoming synapses to gExc
-	float alphaPlus = 0.1f, tauPlus = 20.0f, alphaMinus = 0.1f, tauMinus = 20.0f;
-	sim.setESTDP(gExc, true, STANDARD, ExpCurve(alphaPlus, tauPlus, -alphaMinus, tauMinus));
-	sim.setISTDP(gExc, true, STANDARD, ExpCurve(-alphaPlus, tauPlus, alphaMinus, tauMinus));
+	sim.connect(gInput, gExc, "full", RangeWeight(wtExc), 1.0, RangeDelay(1), RadiusRF(-1), SYN_FIXED);
 
 	// run CUBA mode
 	sim.setConductances(false);
-
 
 	// ---------------- SETUP STATE -------------------
 	sim.setupNetwork();
 
 	SpikeMonitor* SMexc = sim.setSpikeMonitor(gExc, "DEFAULT");
-	SpikeMonitor* SMinh = sim.setSpikeMonitor(gInh, "DEFAULT");
-	ConnectionMonitor* CMee = sim.setConnectionMonitor(gExc, gExc, "DEFAULT");
-	ConnectionMonitor* CMei = sim.setConnectionMonitor(gInh, gExc, "DEFAULT");
+	NeuronMonitor* NMexc = sim.setNeuronMonitor(gExc, "DEFAULT");
 
 	// ---------------- RUN STATE -------------------
 	SMexc->startRecording();
-	SMinh->startRecording();
-	for (int t=0; t<10000; t++) {
-		// random thalamic input to a single neuron from either gExc or gInh
-		std::vector<float> thalamCurrExc(nNeurExc, 0.0f);
-		std::vector<float> thalamCurrInh(nNeurInh, 0.0f);
-		int randNeurId = floor(drand48()*(nNeur-1) + 0.5);
-		float thCurr = 20.0f;
-		if (randNeurId < nNeurExc) {
-			// neurId belongs to gExc
-			thalamCurrExc[randNeurId] = thCurr;
-		} else {
-			// neurId belongs to gInh
-			thalamCurrInh[randNeurId - nNeurExc] = thCurr;
-		}
+	NMexc->startRecording();
+
+	// random thalamic input to a single neuron from either gExc or gInh
+	std::vector<float> thalamCurrExc(ONE_NEURON, 10.0f);
+	sim.setExternalCurrent(gExc, thalamCurrExc);
+
+	//for (int t = 0; t < 500; t++) {
+
 	
-		sim.setExternalCurrent(gExc, thalamCurrExc);
-		sim.setExternalCurrent(gInh, thalamCurrInh);
 	
 		// run for 1 ms, don't generate run stats
-		sim.runNetwork(0,1,false);
-	}
+		sim.runNetwork(0,200,false);
+	//}
 	SMexc->stopRecording();
-	SMinh->stopRecording();
+	NMexc->stopRecording();
 
 	// print firing stats (but not the exact spike times)
 	SMexc->print(false);
-	SMinh->print(false);
-
-	CMee->printSparse();
-	CMei->printSparse();
+	NMexc->print();
 
 	return 0;
 }
