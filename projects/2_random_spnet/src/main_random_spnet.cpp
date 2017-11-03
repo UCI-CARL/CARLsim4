2bd7c0824639712e3cacad7b5f47a25a50f150a8
Hirak <kashyaph@uci.edu>
2017-11-02 22:37:53 -0700

    add 4 group 80-20

--- projects/2_random_spnet/src/main_random_spnet.cpp
+++ projects/2_random_spnet/src/main_random_spnet.cpp
@@ -57,14 +57,20 @@ int main(int argc, const char* argv[]) {
 	CARLsim sim("spnet", CPU_MODE, USER, 2, 42);
 
 	int nNeur = 1000;			// number of neurons
-	int nNeurExc = 0.8*nNeur;	// number of excitatory neurons
+	int nNeurExc1 = 0.267*nNeur;	// number of excitatory-1 neurons
+	int nNeurExc2 = 0.267*nNeur;	// number of excitatory-2 neurons
+	int nNeurExc3 = 0.267*nNeur;	// number of excitatory-3 neurons
 	int nNeurInh = 0.2*nNeur;	// number of inhibitory neurons
 	int nSynPerNeur = 100;  	// number of synpases per neuron
 	int maxDelay = 20;      	// maximal conduction delay
 
 	// create 80-20 network with 80% RS and 20% FS neurons
-	int gExc = sim.createGroup("exc", nNeurExc, EXCITATORY_NEURON, 0, CPU_CORES);
-	sim.setNeuronParameters(gExc, 0.02f, 0.2f, -65.0f, 8.0f); // RS
+	int gExc1 = sim.createGroup("exc1", nNeurExc1, EXCITATORY_NEURON, 0, CPU_CORES);
+	sim.setNeuronParameters(gExc1, 0.02f, 0.2f, -65.0f, 8.0f); // RS1
+	int gExc2 = sim.createGroup("exc2", nNeurExc2, EXCITATORY_NEURON, 0, CPU_CORES);
+	sim.setNeuronParameters(gExc2, 0.02f, 0.2f, -65.0f, 8.0f); // RS2
+	int gExc3 = sim.createGroup("exc3", nNeurExc3, EXCITATORY_NEURON, 0, CPU_CORES);
+	sim.setNeuronParameters(gExc3, 0.02f, 0.2f, -65.0f, 8.0f); // RS3
 	int gInh = sim.createGroup("inh", nNeurInh, INHIBITORY_NEURON, 1, CPU_CORES);
 	sim.setNeuronParameters(gInh, 0.1f, 0.2f, -65.0f, 2.0f); // FS
 
@@ -76,17 +82,36 @@ int main(int argc, const char* argv[]) {
 
 	// gExc receives input from nSynPerNeur neurons from both gExc and gInh
 	// every neuron in gExc should receive ~nSynPerNeur synapses
-	sim.connect(gExc, gExc, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
-	sim.connect(gInh, gExc, "random", RangeWeight(0.0f, wtInh, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+	sim.connect(gExc1, gExc1, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+	sim.connect(gExc1, gExc2, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+	sim.connect(gExc1, gExc3, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+
+	sim.connect(gExc2, gExc1, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+	sim.connect(gExc2, gExc2, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+	sim.connect(gExc2, gExc3, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+
+	sim.connect(gExc3, gExc1, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+	sim.connect(gExc3, gExc2, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+	sim.connect(gExc3, gExc3, "random", RangeWeight(0.0f, wtExc, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+
+	sim.connect(gInh, gExc1, "random", RangeWeight(0.0f, wtInh, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+	sim.connect(gInh, gExc2, "random", RangeWeight(0.0f, wtInh, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
+	sim.connect(gInh, gExc3, "random", RangeWeight(0.0f, wtInh, wtMax), pConn, RangeDelay(1,20), RadiusRF(-1), SYN_PLASTIC);
 
 	// gInh receives input from nSynPerNeur neurons from gExc, all delays are 1ms, no plasticity
 	// every neuron in gInh should receive ~nSynPerNeur synapses
-	sim.connect(gExc, gInh, "random", RangeWeight(wtExc), pConn*nNeur/nNeurExc, RangeDelay(1), RadiusRF(-1), SYN_FIXED);
+	sim.connect(gExc1, gInh, "random", RangeWeight(wtExc), pConn*nNeur/(3*nNeurExc1), RangeDelay(1), RadiusRF(-1), SYN_FIXED);
+	sim.connect(gExc2, gInh, "random", RangeWeight(wtExc), pConn*nNeur/(3*nNeurExc2), RangeDelay(1), RadiusRF(-1), SYN_FIXED);
+	sim.connect(gExc3, gInh, "random", RangeWeight(wtExc), pConn*nNeur/(3*nNeurExc3), RangeDelay(1), RadiusRF(-1), SYN_FIXED);
 
 	// enable STDP on all incoming synapses to gExc
 	float alphaPlus = 0.1f, tauPlus = 20.0f, alphaMinus = 0.1f, tauMinus = 20.0f;
-	sim.setESTDP(gExc, true, STANDARD, ExpCurve(alphaPlus, tauPlus, -alphaMinus, tauMinus));
-	sim.setISTDP(gExc, true, STANDARD, ExpCurve(-alphaPlus, tauPlus, alphaMinus, tauMinus));
+	sim.setESTDP(gExc1, true, STANDARD, ExpCurve(alphaPlus, tauPlus, -alphaMinus, tauMinus));
+	sim.setISTDP(gExc1, true, STANDARD, ExpCurve(-alphaPlus, tauPlus, alphaMinus, tauMinus));
+	sim.setESTDP(gExc2, true, STANDARD, ExpCurve(alphaPlus, tauPlus, -alphaMinus, tauMinus));
+	sim.setISTDP(gExc2, true, STANDARD, ExpCurve(-alphaPlus, tauPlus, alphaMinus, tauMinus));
+	sim.setESTDP(gExc3, true, STANDARD, ExpCurve(alphaPlus, tauPlus, -alphaMinus, tauMinus));
+	sim.setISTDP(gExc3, true, STANDARD, ExpCurve(-alphaPlus, tauPlus, alphaMinus, tauMinus));
 
 	// run CUBA mode
 	sim.setConductances(false);
@@ -95,43 +120,67 @@ int main(int argc, const char* argv[]) {
 	// ---------------- SETUP STATE -------------------
 	sim.setupNetwork();
 
-	SpikeMonitor* SMexc = sim.setSpikeMonitor(gExc, "DEFAULT");
+	SpikeMonitor* SMexc1 = sim.setSpikeMonitor(gExc1, "DEFAULT");
+	SpikeMonitor* SMexc2 = sim.setSpikeMonitor(gExc2, "DEFAULT");
+	SpikeMonitor* SMexc3 = sim.setSpikeMonitor(gExc3, "DEFAULT");
 	SpikeMonitor* SMinh = sim.setSpikeMonitor(gInh, "DEFAULT");
-	ConnectionMonitor* CMee = sim.setConnectionMonitor(gExc, gExc, "DEFAULT");
-	ConnectionMonitor* CMei = sim.setConnectionMonitor(gInh, gExc, "DEFAULT");
+	//ConnectionMonitor* CMee = sim.setConnectionMonitor(gExc, gExc, "DEFAULT");
+	//ConnectionMonitor* CMei = sim.setConnectionMonitor(gInh, gExc, "DEFAULT");
 
 	// ---------------- RUN STATE -------------------
-	SMexc->startRecording();
+	SMexc1->startRecording();
+	SMexc2->startRecording();
+	SMexc3->startRecording();
 	SMinh->startRecording();
 	for (int t=0; t<10000; t++) {
 		// random thalamic input to a single neuron from either gExc or gInh
-		std::vector<float> thalamCurrExc(nNeurExc, 0.0f);
+		std::vector<float> thalamCurrExc1(nNeurExc1, 0.0f);
+		std::vector<float> thalamCurrExc2(nNeurExc2, 0.0f);
+		std::vector<float> thalamCurrExc3(nNeurExc3, 0.0f);
 		std::vector<float> thalamCurrInh(nNeurInh, 0.0f);
 		int randNeurId = floor(drand48()*(nNeur-1) + 0.5);
 		float thCurr = 20.0f;
-		if (randNeurId < nNeurExc) {
+		if (randNeurId < nNeurExc1) {
 			// neurId belongs to gExc
-			thalamCurrExc[randNeurId] = thCurr;
-		} else {
+			thalamCurrExc1[randNeurId] = thCurr;
+		} 
+		
+		else if (randNeurId < (nNeurExc1 + nNeurExc2)) {
+			// neurId belongs to gExc
+			thalamCurrExc2[randNeurId - nNeurExc1] = thCurr;
+		} 
+
+		else if (randNeurId < (nNeurExc1 + nNeurExc2 + nNeurExc3)) {
+			// neurId belongs to gExc
+			thalamCurrExc3[randNeurId - (nNeurExc1 + nNeurExc2)] = thCurr;
+		} 
+
+		else {
 			// neurId belongs to gInh
-			thalamCurrInh[randNeurId - nNeurExc] = thCurr;
+			thalamCurrInh[randNeurId - (nNeurExc1 + nNeurExc2 + nNeurExc3)] = thCurr;
 		}
 	
-		sim.setExternalCurrent(gExc, thalamCurrExc);
+		sim.setExternalCurrent(gExc1, thalamCurrExc1);
+		sim.setExternalCurrent(gExc2, thalamCurrExc2);
+		sim.setExternalCurrent(gExc3, thalamCurrExc3);
 		sim.setExternalCurrent(gInh, thalamCurrInh);
 	
 		// run for 1 ms, don't generate run stats
 		sim.runNetwork(0,1,false);
 	}
-	SMexc->stopRecording();
+	SMexc1->stopRecording();
+	SMexc2->stopRecording();
+	SMexc3->stopRecording();
 	SMinh->stopRecording();
 
 	// print firing stats (but not the exact spike times)
-	SMexc->print(false);
+	SMexc1->print(false);
+	SMexc2->print(false);
+	SMexc3->print(false);
 	SMinh->print(false);
 
-	CMee->printSparse();
-	CMei->printSparse();
+	//CMee->printSparse();
+	//CMei->printSparse();
 
 	return 0;
 }
