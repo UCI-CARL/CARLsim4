e46afdc738591bf20a0d2f06f8218842c144589f
adarshabalaji <adarshabalaji@users.noreply.github.com>
2020-07-17 11:39:53 -0400

    Update build.sh
    build.sh file updated to indicate only NOCUDA support

--- pyCARL/source/build.sh
+++ pyCARL/source/build.sh
@@ -4,13 +4,9 @@ swig -c++ -python carlsim.i
 mkdir -p bin/
 mv carlsim.py bin/
 
-# TO BUILD FOR PYTHON2
+# TO BUILD FOR PYTHON2 WITHOUT CUDA SUPPORT
 #python2 setup.py build_ext -b ./bin/ -t ./bin/wrap --define __NO_CUDA__ --include <CARLSIM_INSTALL_DIR>/CARL/include:/usr/local/cuda/include:/usr/local/cuda/samples/common/inc
 
-# TO BUILD FOR PYTHON3
+# TO BUILD FOR PYTHON3 WITHOUT CUDA SUPPORT
 # swig -c++ -python -py3 carlsim.i
 python3 setup.py build_ext -b ./bin/ -t ./bin/wrap --define __NO_CUDA__ --include <CARLSIM_INSTALL_DIR>/CARL/include:/usr/local/cuda/include:/usr/local/cuda/samples/common/inc
-
-# TO BUILD FOR CUDA SUPPORT
-# TODO: Figure out what needs including to fix errors
-#python3 setup.py build_ext -b ./bin/ -t ./bin/wrap --define __CUDA9__ --include <CARLSIM_INSTALL_DIR>/CARL/include:/usr/local/cuda/include:/usr/local/cuda/samples/common/inc
