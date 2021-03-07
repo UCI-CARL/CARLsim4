49e704e84479f47271f58b09b5fa2a2d5660483b
jkopsick <43210250+jkopsick@users.noreply.github.com>
2021-03-07 15:29:02 -0500

    Update slurm_wrapper.sh

--- projects/synchronous/ca3_snn_GPU_02_16_20_HC_IM_baseline/slurm_wrapper.sh
+++ projects/synchronous/ca3_snn_GPU_02_16_20_HC_IM_baseline/slurm_wrapper.sh
@@ -3,11 +3,9 @@
 #SBATCH --qos gaqos
 #SBATCH --gres=gpu:1
 #SBATCH --exclude=NODE0[40,50,56]
-## SBATCH --cpus-per-task=1
-## SBATCH --nodelist=NODE077
-#SBATCH --job-name="100_2_ivy_ee_vii15_pt55_vei19_ie_1pt45_vei19_ie_U_0pt55_ee_vii15_U_1pt45_conn_stype_v13_pyrdelay_12_varied_count"
-#SBATCH --output /scratch/jkopsick/CARLsim4STP_hc_11_05_20/projects/11_17_20_pyr_tests/ivy_tests_v3/best_ivy_tests/100_tests/ca3_snn_GPU_11_17_20_HC_IM_peri_pyr_0pt15_ee_vii15_pt55_vei19_ie_1pt45_vei19_ie_U_0pt55_ee_vii15_U_1pt45_conn_stype_v13_pyrdelay_12_varied_count_100_2/HC_IM_11_17_peri_pyr_0pt15_ee_vii15_pt55_vei19_ie_1pt45_vei19_ie_U_0pt55_ee_vii15_U_1pt45_conn_stype_v13_pyrdelay_12_varied_count_100_2.txt
+#SBATCH --job-name="sync_baseline"
+#SBATCH --output /scratch/username/CARLsim4_hc/projects/synchronous/ca3_snn_GPU_02_16_20_HC_IM_baseline/HC_IM_02_16_ca3_snn_sync_baseline.txt
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=jkopsick@gmu.edu
+#SBATCH --mail-user=username@gmu.edu
 #SBATCH --mem=100G
 srun ./ca3_snn_GPU
