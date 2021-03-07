29e8be90db0ea4bae243af532064294a3b2636e0
jkopsick <43210250+jkopsick@users.noreply.github.com>
2021-03-07 15:32:00 -0500

    Update slurm_wrapper.sh

--- projects/asynchronous/ca3_snn_GPU_02_16_20_HC_IM_archetype/slurm_wrapper.sh
+++ projects/asynchronous/ca3_snn_GPU_02_16_20_HC_IM_archetype/slurm_wrapper.sh
@@ -3,11 +3,9 @@
 #SBATCH --qos gaqos
 #SBATCH --gres=gpu:1
 #SBATCH --exclude=NODE0[40,50,56]
-## SBATCH --cpus-per-task=1
-## SBATCH --nodelist=NODE077
-#SBATCH --job-name="best_ivy_variant_delayed_activation_1000_1"
-#SBATCH --output /scratch/jkopsick/CARLsim4STP_hc_11_05_20/projects/11_30_20_pyr_tests/delayed_activation/ca3_snn_GPU_11_30_20_HC_IM_best_ivy_variant_delayed_activation_1000_1/HC_IM_11_30_best_ivy_variant_delayed_activation_1000_1.txt
+#SBATCH --job-name="async_archetype"
+#SBATCH --output /scratch/username/CARLsim4_hc/projects/asynchronous/ca3_snn_GPU_02_16_20_HC_IM_archetype/HC_IM_02_16_ca3_snn_async_archetype.txt
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=jkopsick@gmu.edu
+#SBATCH --mail-user=username@gmu.edu
 #SBATCH --mem=100G
 srun ./ca3_snn_GPU
