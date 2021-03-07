181e65c2bb79563088e44771320968e19d9636a7
jkopsick <43210250+jkopsick@users.noreply.github.com>
2021-03-07 15:25:44 -0500

    Update slurm_wrapper.sh

--- projects/ca3_example_net_02_26_21/slurm_wrapper.sh
+++ projects/ca3_example_net_02_26_21/slurm_wrapper.sh
@@ -3,11 +3,9 @@
 #SBATCH --qos gaqos
 #SBATCH --gres=gpu:1
 #SBATCH --exclude=NODE0[40,50,56]
-## SBATCH --cpus-per-task=1
-## SBATCH --nodelist=NODE077
 #SBATCH --job-name="ca3_ex_net"
 #SBATCH --output /scratch/username/CARLsim4_hc/projects/ca3_example_net_02_26_21/HC_IM_02_26_ca3_example_net_results.txt
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=jkopsick@gmu.edu
+#SBATCH --mail-user=username@gmu.edu
 #SBATCH --mem=10G
 srun ./ca3_snn_GPU
