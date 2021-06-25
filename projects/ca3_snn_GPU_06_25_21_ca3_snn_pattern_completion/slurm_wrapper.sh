91eb2d817e68c6ace5ea41f5fddb38e4059dfe04
jkopsick <43210250+jkopsick@users.noreply.github.com>
2021-06-25 17:01:48 -0400

    Update slurm_wrapper.sh

--- projects/ca3_snn_GPU_06_25_21_ca3_snn_pattern_completion/slurm_wrapper.sh
+++ projects/ca3_snn_GPU_06_25_21_ca3_snn_pattern_completion/slurm_wrapper.sh
@@ -5,8 +5,8 @@
 #SBATCH --exclude=NODE0[40,50,56]
 ## SBATCH --cpus-per-task=1
 ## SBATCH --nodelist=NODE077
-#SBATCH --job-name="save_test"
-#SBATCH --output /scratch/jkopsick/CARLsim4STP_hc_02_26_21/projects/afferent_tests/scaled_down_save_load_tests/ca3_snn_GPU_04_13_21_HC_IM_afferents_scaled_ee_save_test/HC_IM_04_13_21_save_test.txt
+#SBATCH --job-name="patcomp"
+#SBATCH --output /scratch/jkopsick/CARLsim4STP_hc_02_26_21/projects/ca3_snn_GPU_06_25_21_ca3_snn_pattern_completion/06_25_21_pattern_completion.txt
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=jkopsick@gmu.edu
 #SBATCH --mem=10G
